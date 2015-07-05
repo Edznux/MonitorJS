55c6a2c1584a72a4c4c1424fe7067607b174850c
Edznux <edznux@gmail.com>
2015-07-06 01:33:36 +0200

    fix grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,19 +6,27 @@ grunt.initConfig({
   uglify: {
     all_src : {
       options : {
-        sourceMap : true,
+        sourceMap : false,
+        // sourceMap : true,
         sourceMapName : 'sourceMap.map'
       },
       src : 'assets/*.js',
-      dest : 'public/js/frontend.min.js'
+      dest : 'public/js/frontend.js'
+    }
+  },
+  watch: {
+    js: {
+      files: ['assets/*.js'],
+      tasks: ['uglify']
     }
   }
 });
 
+
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
-
 };
