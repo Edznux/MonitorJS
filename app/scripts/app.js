1b286bedb46ac2ecf14ebf324e6a54eb6b158938
Edouard SCHWEISGUTH <edznux@gmail.com>
2015-07-06 03:57:46 +0200

    Add link (state) and create views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,36 @@
-var app = angular.module('monitor', ['ngMaterial']);
+var app = angular.module('monitor', ['ngMaterial','ui.router']);
+
+app.config(['$stateProvider', monitorRouter]);
+
+function monitorRouter($stateProvider) {
+	$stateProvider
+		.state({
+			name : 'home',
+			templateUrl: '../views/main.html'
+		})
+		.state({
+			name : 'cpu',
+			templateUrl : '../views/cpu.html'
+		})
+		.state({
+			name : 'memory',
+			templateUrl : '../views/memory.html'
+		})
+		.state({
+			name : 'disk',
+			templateUrl : '../views/disk.html'
+		})
+		.state({
+			name : 'network',
+			templateUrl : '../views/network.html'
+		})
+};
+
+app.config(['$stateProvider', analyticsRouter]);
+function analyticsRouter($stateProvider) {
+	$stateProvider
+		.state({
+			name : 'analytics',
+			templateUrl: '../views/analytics/main.html'
+		})
+};
